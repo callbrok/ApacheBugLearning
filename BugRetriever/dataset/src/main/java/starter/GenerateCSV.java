bc40816ce8190ee1cf6cf7e5fb9a4f3f132a7a81
Marco Purificato <63931260+callbrok@users.noreply.github.com>
2023-09-28 17:49:08 +0200

    Upload Sonar Cloud Overview

--- BugRetriever/dataset/src/main/java/starter/GenerateCSV.java
+++ BugRetriever/dataset/src/main/java/starter/GenerateCSV.java
@@ -4,13 +4,7 @@ import controller.*;
 import model.*;
 
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.logging.Level;
